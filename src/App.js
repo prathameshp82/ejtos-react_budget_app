5a209b5cbdbe1e4bebac300f29f37dd83bc111b3
prathameshp82 <132730079+prathameshp82@users.noreply.github.com>
2023-05-27 16:35:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
-import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseItem from './components/ExpenseItem';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-import { AppProvider } from './context/AppContext';
+import RemainingBudget from './components/Remaining';
+
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        { // Budget component
-                    <div className='col-sm'>
-                        <Budget />
-                    </div>}        
-
-                        { //Remaining component
-                    <div className='col-sm'>
-                        <Remaining />
-                    </div>}        
-
-                        { //ExpenseTotal component
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>}        
-                       
-                        {//ExpenseList component 
-                     <div className='col-sm'>
-                     <ExpenseList />
-                     </div>}         
-
-                        { //ExpenseItem component
-                     <div className='col-sm'>
-                     <ExpenseItem />
-                     </div>}        
-
-                        {//AllocationForm component
-                     <div className='col-sm'>
-                     <AllocationForm />
-                     </div>}        
-
-                </div>
-            </div>
-        </AppProvider>
-    );
+	return (
+		<AppProvider>
+			<div className='container'>
+				<h1 className='mt-3'>Company's Budget Allocation</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<Budget />
+					</div>
+					<div className='col-sm'>
+						<RemainingBudget />
+					</div>
+					<div className='col-sm'>
+						<ExpenseTotal />
+					</div>
+				</div>
+				<h3 className='mt-3'>Allocation</h3>
+				<div className='row '>
+					<div className='col-sm'>
+						<ExpenseList />
+					</div>
+				</div>
+				<h3 className='mt-3'>Change allocation</h3>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<AllocationForm/>
+					</div>
+				</div>
+			</div>
+		</AppProvider>
+	);
 };
+
 export default App;
